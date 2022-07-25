779b6a6410a9f45bb09c1f97b12c5ffa5cfc5d03
Tan, Faith <jyt3@illinois.edu>
2022-07-25 13:02:32 +0800

    commit

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-import numpy as np
+# import numpy as np
 
-all_embeddings = np.load('embeddings-temp.npy')
+# all_embeddings = np.load('embeddings-temp.npy')
 
-for emb in all_embeddings:
-    print(emb)
-    print("__________")
+# for emb in all_embeddings:
+#     print(emb)
+#     print("__________")
+
+import tensorflow_hub as hub
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+print(embed(["hi"]))
